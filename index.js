4e3f70973107b8c6d8b546ba6c6b971bbb92508b
¨kericho¨ <lvisrono90@gmail.com>
2022-11-26 12:54:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,38 +1,30 @@
-const cats = (["Milo", "Otis", "Garfield"])
-function desctructivelyRemoveLastCat(){
-    Cats .pop("");
-    const copyOfCats = [...cats]
-}
-function destructivelyAppendCat(){
-    cats .push("Ralph");
-    const copyOfCats = [...cats];
-}
-function destructivelyPrependCat(){
-    cats .unshift("Bob");
-    const copyOfCats = [...cats];
-}
-function destructivelyRemoveLastCat(){
-    cats .pop("");
-    const copyOfCats = [...cats];
-}
-function destructivelyRemoveFirstCat(){
-    cats .shift("");
-    const copyOfCats = [...cats];
-}
-const cat = ["Milo", "Otis", "Garfield"];
-function appendCat(){
-    var newArray = cat.slice();
-    // or ES6 way
-    // var newArray = [...cats];
-    newArray.push("Broom");
-    return newArray
+// Write your solution here!
+const cats= ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+    return cats;
+ }
+    function destructivelyPrependCat(name){
+        return cats.unshift(name);
+ }
+function destructivelyRemoveLastCat(name){
+        return cats.pop(name);
+ }
+function destructivelyRemoveFirstCat(name){
+            return  cats.shift(name);
+ }
+function appendCat(name){
+            return [...cats,name];
+          
 }
 function prependCat(name){
-    return[...cats.slice(0, 0), "Arnold", "Milo", "Otis", "Garfield"];
-}
-function removeLastCat(){
-    return cats.slice(0, cats.length -1);
+    return [name,...cats];
+ }
+ function removeLastCat(){
+    return cats.slice(0,-1);
+  
 }
 function removeFirstCat(){
-    return cats.slice(1)
-}
+    return cats.slice(1);
+  }
